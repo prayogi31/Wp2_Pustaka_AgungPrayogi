c8c55621ad74485e6b2ae4456c49e3ba5903142f
Prayogi <agungnoid12@gmail.com>
2021-10-24 20:45:28 +0700

    initial commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,16 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "<h3>Nama saya Agung Prayogi,
+         <br>Saya Tinggal Di PULO MAS Jakarta Timur
+          <br>Olah Raga Yang di Sukai adalah SepakBola<h3>";
+    }
+}
+?>
+        
+
+
+ 
---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Agung</li>
+        <li>Nama Belakang : Prayogi</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Pulo Mas, Jakarta Timur</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Jakarta Timur</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Brenang</li>
+        <li>Motoran</li>
+    </ul>
+</section>
